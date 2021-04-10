aca690575c686072b07db5335f7afdf4faebce0c
SrClauDev <69334014+SrClau@users.noreply.github.com>
2021-04-10 16:14:39 -0300

    update HCFListener.php

--- src/hcf/HCFListener.php
+++ src/hcf/HCFListener.php
@@ -2,7 +2,35 @@
 
 namespace hcf;
 
+use pocketmine\event\{Listener,player\PlayerJoinEvent,player\PlayerCreationEvent,player\PlayerPreLoginEvent,player\PlayerQuitEvent,player\PlayerMoveEvent,player\PlayerChatEvent,player\PlayerInteractEvent,player\PlayerDeathEvent};
+
+use pocketmine\utils\TextFormat as Text;
+
+use pocketmine\entity\Entity;
+
+use pocketmine\level\{Level,Position};
+
+use pocketmine\math\Vector3;
+
+use pocketmine\tile\Sign;
+
+use pocketmine\item\Item;
+
+use pocketmine\event\block\{SignChangeEvent,BlockBreakEvent};
+use pocketmine\{Player};
+
+use hcf\{HCFLoad,PlayerHCF};
+
 class HCFListener implements Listener {
-  
+
+private $hcf;
+
+public function __construct(HCFLoad $load){
+$this->hcf = $load;
+}
+
+public function creationPlayer(PlayerCreationEvent $event){
+$event->setPlayerClass(PlayerHCF::class);
+}
 }
 ?>
