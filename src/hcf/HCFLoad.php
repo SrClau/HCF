11bc90930f834125af0a8a967020a00a51fa4cb8
SrClauDev <69334014+SrClau@users.noreply.github.com>
2021-04-10 12:26:27 -0300

    Added CONST/VARIABLES/BOOLS

--- src/hcf/HCFLoad.php
+++ src/hcf/HCFLoad.php
@@ -10,19 +10,34 @@ use pocketmine\utils\{Config,TextFormat as Text;
 
 class HCFLoad extends PluginBase {
 
-/** @instance HCFLoad|String null **/  
+public const PLUGIN_VERSION = "1.0.0";
+
+public const BORDER_MAP = 2000;
+
+public const MAX_ALLYS = 3;
+
+public const LEVEL_MAX_Y = 128;
+
+public const LEVEL_LESS_Y = 0;
+
+/**@instance HCFLoad|String null **/  
 public static $instance;
 
 public static $kitsManager;
 
 public static $shopManager;
 
+public static $provider;
+
 public $sotw;
 
 public $eotw;
 
 public function onEnable(){
-  
+self::$instance = $this;
+if($version !== self::PLUGIN_VERSION){
+$config->setNested("version", self::PLUGIN_VERSION);
+}
 }
 }
 ?>
