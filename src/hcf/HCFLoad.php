bd3178811454773d5f97b4943f15b3ef6f2fc342
SrClauDev <69334014+SrClau@users.noreply.github.com>
2021-04-10 12:21:03 -0300

    update HCFLoad.php

--- src/hcf/HCFLoad.php
+++ src/hcf/HCFLoad.php
@@ -1,1 +1,28 @@
+<?php
 
+namespace hcf;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\entity\Entity;
+
+use pocketmine\utils\{Config,TextFormat as Text;
+
+class HCFLoad extends PluginBase {
+
+/** @instance HCFLoad|String null **/  
+public static $instance;
+
+public static $kitsManager;
+
+public static $shopManager;
+
+public $sotw;
+
+public $eotw;
+
+public function onEnable(){
+  
+}
+}
+?>
