62b79650675bd7c0089d24598eeb5f598c19ef8d
SrClauDev <69334014+SrClau@users.noreply.github.com>
2021-04-10 12:58:07 -0300

    Added listener HCF

--- src/hcf/HCFLoad.php
+++ src/hcf/HCFLoad.php
@@ -81,6 +81,7 @@ $version = $config->getNested("version");
 if($version !== self::PLUGIN_VERSION){
 $config->setNested("version", self::PLUGIN_VERSION);
    }
+$this->getServer()->getPluginManager()->registerEvents(new HCFListener($this), $this);
 $this->getScheduler()->scheduleRepeatingTask(new MOTDTask($this, $config->get("server-name"), $config->get("message-motd")), 3600);
 }
 
