d69ce174d3b5912233d1698241820b5952dbbce2
SrClauDev <69334014+SrClau@users.noreply.github.com>
2021-04-09 21:52:03 -0300

    Update LogoutZombie.php

--- src/hcf/fight/logger/LogoutZombie.php
+++ src/hcf/fight/logger/LogoutZombie.php
@@ -1,1 +1,15 @@
+<?php
 
+namespace hcf\fight\logger;
+
+use hcf\HCFLoad;
+use hcf\PlayerHCF;
+
+use pocketmine\item\Item;
+
+use pocketmine\nbt\NBT;
+use pocketmine\nbt\tag\{StringTag, ListTag};
+class LogoutZombie extends Zombie {
+
+}
+?>
