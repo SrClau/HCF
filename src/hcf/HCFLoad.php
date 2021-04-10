712903f5e4064ab5ab003c8f07320b9236ef00d0
SrClauDev <69334014+SrClau@users.noreply.github.com>
2021-04-10 12:43:58 -0300

    Added files in HCF

--- src/hcf/HCFLoad.php
+++ src/hcf/HCFLoad.php
@@ -6,15 +6,23 @@ use pocketmine\plugin\PluginBase;
 
 use pocketmine\entity\Entity;
 
-use pocketmine\utils\{Config,TextFormat as Text;
+use pocketmine\utils\{Config,TextFormat as Text};
 
 class HCFLoad extends PluginBase {
 
-public const PLUGIN_VERSION = "1.0.0";
+public const GAME_MODE = "HardcoreFactions"; //change to your liking
 
-public const BORDER_MAP = 2000;
+public const PLUGIN_VERSION = "1.0.0"; //change to your liking
 
-public const MAX_ALLYS = 3;
+public const BORDER_MAP = 2000; //FIX: error when resizing the border to your liking
+
+public const MAX_PROTECTION = 1; //change to your liking
+
+public const MAX_SHARPNESS = 1; //change to your liking
+
+public const MAX_POWER = 1; //change to your liking
+
+public const MAX_ALLYS = 3; //change to your liking
 
 public const LEVEL_MAX_Y = 128;
 
@@ -27,7 +35,13 @@ public static $kitsManager;
 
 public static $shopManager;
 
-public static $provider;
+public static $cratesManager;
+
+public static $factionManager;
+
+public static $yaml;
+
+public static $sql;
 
 public $sotw;
 
@@ -35,9 +49,38 @@ public $eotw;
 
 public function onEnable(){
 self::$instance = $this;
+@mkdir($this->getDataFolder());
+@mkdir($this->getDataFolder() . "crates/");
+@mkdir($this->getDataFolder() . "players/");
+@mkdir($this->getDataFolder() . "signs/");
+@mkdir($this->getDataFolder() . "data/");
+if(!is_file($this->getDataFolder() . "config.yml")){
+$this->saveResource("config.yml");
+}
+if(!is_file($this->getDataFolder() . "listcrates.yml")){
+$this->saveResource("listcrates.yml");
+}
+if(!is_file($this->getDataFolder() . "listitems.yml")){
+$this->saveResource("listitems.yml");
+}
+if(!is_file($this->getDataFolder() . "messages.yml")){
+$this->saveResource("messages.yml");
+}
+if(!is_file($this->getDataFolder() . "scoreboard.json")){
+$this->saveResource("scoreboard.json");
+}
+self::$yaml = new YamlProvider($this);
+//self::$sql = new MySQLProvider($this);
+
+$config = $this->getConfig();
+$version = $config->getNested("version");
 if($version !== self::PLUGIN_VERSION){
 $config->setNested("version", self::PLUGIN_VERSION);
+   }
 }
+
+public function isSOTW(): bool {
+  
 }
 }
 ?>
