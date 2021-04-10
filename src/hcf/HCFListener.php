091e7b904e5935d452db58ea1e589d91d06b4479
SrClauDev <69334014+SrClau@users.noreply.github.com>
2021-04-10 16:44:06 -0300

    update HCFListener.php

--- src/hcf/HCFListener.php
+++ src/hcf/HCFListener.php
@@ -17,6 +17,7 @@ use pocketmine\tile\Sign;
 use pocketmine\item\Item;
 
 use pocketmine\event\block\{SignChangeEvent,BlockBreakEvent};
+
 use pocketmine\{Player};
 
 use hcf\{HCFLoad,PlayerHCF};
@@ -31,6 +32,42 @@ $this->hcf = $load;
 
 public function creationPlayer(PlayerCreationEvent $event){
 $event->setPlayerClass(PlayerHCF::class);
+}
+
+public function signChange(SignChangeEvent $event){
+$lines = $event->getLines();
+if($lines[0] === "[elevator]"){
+$elevator = strtolower($lines[1]);
+ if($elevator === "up" || $elevator === "down"){
+  if($lines[2] === "" || $lines[2] === ""){
+  $event->setLine(0, Text::BOLD . Text::DARK_GRAY . "[" . Text::AQUA . "Elevator" . Text::DARK_GRAY . "]");
+  $event->setLine(1, Text::YELLOW . ucfirst($lines[1]));
+         }
+      } 
+   } 
+}
+
+public function onbreak(BlockBreakEvent $event){
+$player = $event->getPlayer(); //object: PlayerHCF
+$block = $event->getBlock(); 
+if($player->hasEffect()){
+$event->setCancelled(true);
+return;
+}
+if(($block->getId() === Block::DIAMOND_ORE) && !isset($this->blocks[])){
+$diamond = 0;
+ for($x = $block->getX()-5; $x <= $block->getX() + 4; $x++){
+   for($z = $block->getZ()-5; $z <= $block->getZ() + 4; $z++){
+     for($y = $block->getY()-5; $y <= $block->getY() + 4; $y++){
+       if(($player->getLevel()->getBlockIdAt($x, $y, $z)) === Block::DIAMOND_ORE && !isset($this->blocks[])){
+     $this->blocks[] = true;
+     $diamond++;  
+       }
+     }
+   }
+ }
+$this->hcf->getServer()->broadcastMessage(MessageManager::sendMessage("foundDiamond", ["name" => Text::YELLOW . $player->getName(), "amount" => Text::BLUE . $diamond]));
+   }
 }
 }
 ?>
