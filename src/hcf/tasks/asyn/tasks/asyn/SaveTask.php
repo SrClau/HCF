0f0b05c4b47fdfefe662390e8a68ce211d97d7a5
SrClauDev <69334014+SrClau@users.noreply.github.com>
2021-04-11 14:50:32 -0300

    update SaveTask.php

--- src/hcf/tasks/asyn/tasks/asyn/SaveTask.php
+++ src/hcf/tasks/asyn/tasks/asyn/SaveTask.php
@@ -11,6 +11,34 @@ use pocketmine\Server;
 use pocketmine\utils\TextFormat;
 
 class SaveTask extends AsyncTask {
-  
+/** @here string **/
+private $name = "";
+/** @here array **/
+private $kits;
+/** @here array **/
+private $data;
+/** @here array **/
+private $cooldown;
+/** @here array **/
+private $pvp;
+/** 
+* @constructor SaveTask
+* @param $name string
+* @param $kits array
+* @param $data array
+* @param $cooldown array
+* @param $pvp array
+**/
+public function __construct(string $name, array $kits, array $data, array $cooldown, array $pvp){
+$this->name = $name;
+$this->kits = $kits;
+$this->data = $data;
+$this->cooldown = $cooldown;
+$this->pvp = $pvp;
+}
+
+public function onRun(): void {
+
+}
 }
 ?>
