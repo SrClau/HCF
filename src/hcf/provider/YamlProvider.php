368f5f6d2ff394b19c46c5c9b83cb3f573652355
SrClauDev <69334014+SrClau@users.noreply.github.com>
2021-04-11 14:12:35 -0300

    update YamlProvider.php

--- src/hcf/provider/YamlProvider.php
+++ src/hcf/provider/YamlProvider.php
@@ -1,1 +1,25 @@
+<?php
 
+namespace hcf\provider;
+
+use pocketmine\utils\Config;
+
+use hcf\HCFLoad;
+
+class YamlProvider {
+ 
+/** @here HCFLoad **/ 
+public $load = null;
+
+public function __construct(HCFLoad $main){
+$this->load = $main;
+$this->init();
+}
+
+public function init(): void {
+if(!is_dir($this->load->getDataFolder() . "crates/")){
+  
+}
+}
+}
+?>
