b8cfc9174bfaca016104886b1f40042b6d603087
SrClauDev <69334014+SrClau@users.noreply.github.com>
2021-04-11 14:35:48 -0300

    update SaveTask.php

---
+++ src/hcf/tasks/asyn/tasks/asyn/SaveTask.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace hcf\tasks\asyn;
+
+use hcf\{HCFLoad,PlayerHCF};
+
+use pocketmine\scheduler\AsyncTask;
+
+use pocketmine\Server;
+
+use pocketmine\utils\TextFormat;
+
+class SaveTask extends AsyncTask {
+  
+}
+?>
