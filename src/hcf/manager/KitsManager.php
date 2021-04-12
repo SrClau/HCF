a03de7ef8d61cdabb79e3e62d3157877ecb3d64b
SrClauDev <69334014+SrClau@users.noreply.github.com>
2021-04-12 07:44:54 -0300

    update KitsManager.php

--- src/hcf/manager/KitsManager.php
+++ src/hcf/manager/KitsManager.php
@@ -1,1 +1,21 @@
+<?php
 
+namespace hcf\manager;
+
+use hcf\PlayerHCF;
+
+use pocketmine\utils\TextFormat as Text;
+
+use pocketmine\item\{Item,enchantment\Enchantment,enchantment\EnchantmentInstance};
+
+use pocketmine\tile\{Tile,Sign};
+
+use pocketmine\inventory\transaction\SlotChangeAction;
+
+class KitsManager {
+ 
+public function sendSign(PlayerHCF $player){
+
+}
+}
+?>
