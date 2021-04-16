56709e5a235c19564f0dc6b5459c9ad89d3077bc
Clau <69334014+SrClau@users.noreply.github.com>
2021-04-16 20:27:29 -0300

    update CommandsManager.php

--- src/hcf/commands/CommandsManager.php
+++ src/hcf/commands/CommandsManager.php
@@ -12,6 +12,12 @@ public static function init(): void {
 $instance = HCFLoad::getInstance();
 $server = $instance->getServer();
 $command = $server->getCommandMap();
+/** Staffmode **/
 $command->register("staffmode", new StaffModeCommand($instance));
+/** HCF Test **/
+$command->register("test", new Test($this)) ;
+/** Stats **/
+$command->register("stats", new StatsCommand($this));
+
 }
 }
