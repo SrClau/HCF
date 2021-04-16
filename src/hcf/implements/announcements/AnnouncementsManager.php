4102a4ca14da6f0ab34696fc459cfb947ddae52d
Clau <69334014+SrClau@users.noreply.github.com>
2021-04-16 20:58:35 -0300

    update AnnouncementsManager.php

--- src/hcf/implements/announcements/AnnouncementsManager.php
+++ src/hcf/implements/announcements/AnnouncementsManager.php
@@ -1,1 +1,35 @@
+<?php
 
+namespace hcf\implement\announcements;
+
+use pocketmine\Server;
+
+use pocketmine\utils\{Config,TextFormat};
+
+class AnnouncementsManager {
+ 
+public $hcf;
+
+public $announcements = [];
+
+public $AdsActivated = true;
+
+public $recentAnnouncement = "";
+
+public function __construct(HCFLoad $main){
+$this->hcf = $main;
+/*$main->*/
+}
+
+public function getRecentAnnouncement(): string {
+  
+}
+
+public function addAnnouncement(string $announcement, int $value){
+
+}
+
+public function deleteAnnoucnement(int $value_announcement){
+  
+}
+}
