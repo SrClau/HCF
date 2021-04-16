768bf81c77a272904ce366204b2c400ccdb05ea4
Clau <69334014+SrClau@users.noreply.github.com>
2021-04-16 20:18:19 -0300

    update CommandsManager.php

--- src/hcf/commands/CommandsManager.php
+++ src/hcf/commands/CommandsManager.php
@@ -1,1 +1,17 @@
+<?php
 
+namespace hcf\commands;
+
+use pocketmine\Server;
+
+use hcf\HCFLoad;
+
+class CommandsManager {
+
+public static function init(): void {
+$instance = HCFLoad::getInstance();
+$server = $instance->getServer();
+$command = $server->getCommandMap();
+$command->register("staffmode", new StaffModeCommand($instance));
+}
+}
