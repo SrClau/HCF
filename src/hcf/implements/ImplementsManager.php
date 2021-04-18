3c0d9ec1c59b0961bce13c66d477e89a25a7a3e2
Clau <69334014+SrClau@users.noreply.github.com>
2021-04-18 14:29:49 -0300

    Added AnnouncementsManager in ImplementsManager

--- src/hcf/implements/ImplementsManager.php
+++ src/hcf/implements/ImplementsManager.php
@@ -1,1 +1,12 @@
+<?php
 
+namespace hcf\implement;
+
+use hcf\implement\announcements\AnnouncementManager;
+
+class ImplementsManager {
+  
+public static function init(): void {
+AnnouncementsManager::init();
+}
+}
