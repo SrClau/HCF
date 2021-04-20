b0d91fa84165df04309b7bc114b0442230c5f4ca
Clau <69334014+SrClau@users.noreply.github.com>
2021-04-20 00:49:35 -0300

    update Time.php

--- src/hcf/utils/Time.php
+++ src/hcf/utils/Time.php
@@ -5,11 +5,11 @@ namespace hcf\utils;
 class Time {
 
 public static function intToTime(int $value){
-return gmdate();
+return gmdate("i:s", $int);
 }
 
 public static function intToTimeFull(int $value){
-return gmdate();
+return gmdate("H:i:s", $int);
 }
 }
 ?>
