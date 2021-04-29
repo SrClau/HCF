0698c36e8a50b158bfffc05f749f853391d62984
SrDevYT <69334014+SrClau@users.noreply.github.com>
2021-04-28 21:48:33 -0300

    update ItemManager.php

--- src/hcf/item/ItemManager.php
+++ src/hcf/item/ItemManager.php
@@ -1,1 +1,16 @@
+<?php
 
+namespace hcf\item;
+
+use pocketmine\item\{Item,ItemFactory,enchantment\Enchantment};
+
+use pocketmine\entity\Entity;
+
+use hcf\item\{SnowballBass};
+class ItemManager {
+ 
+public static function load(): void {
+ItemFactory::register(new EnderPearl(), true);
+ItemFactory::registerItem(new SnowballBass(), true);
+   } 
+}
