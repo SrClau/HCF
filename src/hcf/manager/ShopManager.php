6f3b7718a9838541fc241a0c499d0d4be8f1d6ca
SrDevYT <69334014+SrClau@users.noreply.github.com>
2021-05-08 11:35:08 -0300

    update ShopManager.php

--- src/hcf/manager/ShopManager.php
+++ src/hcf/manager/ShopManager.php
@@ -1,1 +1,26 @@
+<?php
 
+namespace hcf\manager;
+
+use pocketmine\item\Item;
+
+use pocketmine\tile\{Tile, Sign};
+
+use pocketmine\math\Vector3;
+
+use pocketmine\utils\{
+Config,
+TextFormat as Text
+};
+
+class ShopManager {
+
+private$hcf;
+
+public $shop;
+
+public function __construct(MythicalHCF $plugin){
+$this->hcf = $plugin;
+$this->shop = new Config($plugin->getDataFolder() . "", Config::);
+}
+}
